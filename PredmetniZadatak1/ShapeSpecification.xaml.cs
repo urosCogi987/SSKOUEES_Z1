480de3e1a89eb436cd63094a94128f61ea10ffe8
urosCogi987 <urosworkonly@gmail.com>
2021-03-19 17:51:06 +0100

    Drawing shapes finished, Image and Routed commands left

--- PredmetniZadatak1/ShapeSpecification.xaml.cs
+++ PredmetniZadatak1/ShapeSpecification.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,8 +22,13 @@ namespace PredmetniZadatak1
     public partial class ShapeSpecification : INotifyPropertyChanged
     {
         #region fields
-        public event PropertyChangedEventHandler PropertyChanged;       
+        public event PropertyChangedEventHandler PropertyChanged;  
+        
         private string objwidth;
+        private string objheight;
+        private Color fillcolor;
+        private Color bordercolor;
+        private string objborderthickness;
         private string typeComponent = String.Empty;
 
         // Starting coordinates
@@ -42,6 +48,58 @@ namespace PredmetniZadatak1
                 {
                     objwidth = value;
                     OnPropertyChanged("ObjWidth");
+                }
+            }
+        }
+
+        public string ObjHeight
+        {
+            get { return objheight; }
+            set
+            {
+                if (objheight != value)
+                {
+                    objheight = value;
+                    OnPropertyChanged("ObjHeight");
+                }
+            }
+        }
+
+        public Color FillColor
+        {
+            get { return fillcolor; }
+            set
+            {
+                if (fillcolor != value )
+                {
+                    fillcolor = value;
+                    OnPropertyChanged("FillColor");
+                }
+            }
+        }
+
+        public Color BorderColor
+        {
+            get { return bordercolor; }
+            set
+            {
+                if (bordercolor != value)
+                {
+                    bordercolor = value;
+                    OnPropertyChanged("BorderColor");
+                }
+            }
+        }
+
+        public string ObjBorderThickness
+        {
+            get { return objborderthickness; }
+            set
+            {
+                if (objborderthickness != value)
+                {
+                    objborderthickness = value;
+                    OnPropertyChanged("ObjBorderThickness");
                 }
             }
         }
@@ -67,11 +125,12 @@ namespace PredmetniZadatak1
 
         #endregion
 
-        // valjda mora da postoji prazan konstruktor
+        #region Constructors
         public ShapeSpecification()
         {
             InitializeComponent();
         }
+        
 
         public ShapeSpecification(string componentType, Point startingPosition, List<Point> polygonPoints = null)
         {
@@ -80,8 +139,13 @@ namespace PredmetniZadatak1
             StartingX = startingPosition.X;
             StartingY = startingPosition.Y;
             PolygonPoints = polygonPoints;
-        }
 
+            fillColorCmbBox.ItemsSource = typeof(Colors).GetProperties();
+            fillColorCmbBox.SelectedIndex = 10;
+            borderColorCmbBox.ItemsSource = typeof(Colors).GetProperties();
+            borderColorCmbBox.SelectedIndex = 11;
+        }
+        #endregion
 
         private void cancelBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -93,14 +157,16 @@ namespace PredmetniZadatak1
             double resultHeight;
 
             MainWindow main = ((MainWindow)Application.Current.MainWindow);
+            SolidColorBrush fillColorCmbBox = new SolidColorBrush(FillColor);
+            SolidColorBrush borderColorCmbBox = new SolidColorBrush(BorderColor);
 
             //if (!Double.TryParse(objWidth.Text, out resultHeight) || resultHeight < 1)
             if (typeComponent == "Ellipse")
             {
                 Ellipse ellipse = new Ellipse();
                 ellipse.Name = "rectangle_PROBA";
-                ellipse.Fill = Brushes.Red;
-                ellipse.Stroke = Brushes.Blue;
+                ellipse.Fill = fillColorCmbBox;
+                ellipse.Stroke = borderColorCmbBox;
                 ellipse.Width = Double.Parse(objWidth.Text);
                 ellipse.Height = Double.Parse(objHeight.Text);
                 ellipse.StrokeThickness = Double.Parse(objBorderThickness.Text);
@@ -113,8 +179,8 @@ namespace PredmetniZadatak1
             {
                 Rectangle rectangle = new Rectangle();
                 rectangle.Name = "rectangle_PROBA";
-                rectangle.Fill = Brushes.Purple;
-                rectangle.Stroke = Brushes.Aqua;
+                rectangle.Fill = fillColorCmbBox;
+                rectangle.Stroke = borderColorCmbBox;
                 rectangle.Width = Double.Parse(objWidth.Text);
                 rectangle.Height = Double.Parse(objHeight.Text);
                 rectangle.StrokeThickness = Double.Parse(objBorderThickness.Text);
@@ -129,13 +195,12 @@ namespace PredmetniZadatak1
                 Polygon polygon = new Polygon();
                 foreach (Point pPoint in polygonPoints)
                 {
-                    polygon.Points.Add(pPoint);
-                    //Console.WriteLine(pPoint.X + " " + pPoint.Y);
+                    polygon.Points.Add(pPoint);                    
                 }
                
                 polygon.Name = "polygon_PROBA";
-                polygon.Fill = Brushes.DarkGreen;
-                polygon.Stroke = Brushes.LightBlue;                
+                polygon.Fill = fillColorCmbBox;
+                polygon.Stroke = borderColorCmbBox;
                 polygon.StrokeThickness = Double.Parse(objBorderThickness.Text);
                 main.MyCanvas.Children.Add(polygon);
 
@@ -150,11 +215,16 @@ namespace PredmetniZadatak1
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        }
+        }        
 
-        private void objWidth_TextChanged(object sender, TextChangedEventArgs e)
+        private void fillColorCmbBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // dal treba ovo, cemu ovo?
+            FillColor = (Color)(fillColorCmbBox.SelectedItem as PropertyInfo).GetValue(null, null);
+        }        
+
+        private void borderColorCmbBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BorderColor = (Color)(borderColorCmbBox.SelectedItem as PropertyInfo).GetValue(null, null);
         }
     }
 }
